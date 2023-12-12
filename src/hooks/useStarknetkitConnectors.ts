328e0156240da1b2b27f3e37499ef74c9984925a
bluecco <bluecco@gmail.com>
2023-12-12 10:36:36 +0100

    chore: refactor useStarknetkitConnectors params

--- src/hooks/useStarknetkitConnectors.ts
+++ src/hooks/useStarknetkitConnectors.ts
@@ -1,29 +1,21 @@
 import { type Connector } from "../connectors"
-import { defaultConnectors } from "../helpers/defaultConnectors"
 import { connect } from "../main"
 import { ConnectOptions } from "../types/modal"
 
 type UseStarknetkitConnectors = {
-  connectors?: Connector[]
+  connectors: Connector[]
   starknetkitConnect: () => Promise<Connector | null>
 }
 
 const useStarknetkitConnectors = (
-  options: ConnectOptions,
+  options: Omit<ConnectOptions, "argentMobileOptions" | "webWalletUrl">,
 ): UseStarknetkitConnectors => {
   const starknetkitConnect = async () => {
     return await connect(options)
   }
 
-  const connectors =
-    options?.connectors ||
-    defaultConnectors({
-      argentMobileOptions: options?.argentMobileOptions,
-      webWalletUrl: options?.webWalletUrl,
-    })
-
   return {
-    connectors,
+    connectors: options.connectors,
     starknetkitConnect,
   }
 }
