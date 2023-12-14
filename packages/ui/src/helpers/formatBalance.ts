0b07b2183aebce1051e037aba9d198a566833833
bluecco <bluecco@gmail.com>
2023-12-14 16:12:47 +0100

    chore: add tests for ui helpers

--- packages/ui/src/helpers/formatBalance.ts
+++ packages/ui/src/helpers/formatBalance.ts
@@ -1,53 +1,18 @@
-export interface Unit {
-  amount: bigint
-  decimals: number
-}
-
 export interface Balance {
   amount: bigint
   decimals: number
   symbol: string
 }
 
-function parseAmount(
-  { amount, decimals }: Unit,
-  decimalsToDisplay: number = decimals,
-): {
-  integerPart: string
-  decimalPart: string
-} {
+export function formatBalance(
+  { amount, decimals, symbol }: Balance,
+  decimalsToDisplay: number = 4,
+): string {
   const bigIntString = amount.toString().padStart(decimals + 1, "0")
   const integerPart = bigIntString.slice(0, bigIntString.length - decimals)
   const decimalPart = bigIntString.slice(
     bigIntString.length - decimals,
     bigIntString.length - decimals + decimalsToDisplay,
-  )
-
-  return {
-    integerPart,
-    decimalPart,
-  }
-}
-
-export function formatUnit(
-  { amount, decimals }: Unit,
-  decimalsToDisplay?: number,
-): string {
-  const { integerPart, decimalPart } = parseAmount(
-    { amount, decimals },
-    decimalsToDisplay,
-  )
-
-  return `${integerPart}.${decimalPart}`.replace(/\.?0+$/, "")
-}
-
-export function formatBalance(
-  { amount, decimals, symbol }: Balance,
-  decimalsToDisplay: number = 4,
-): string {
-  const { integerPart, decimalPart } = parseAmount(
-    { amount, decimals },
-    decimalsToDisplay,
   )
 
   const fullNumber = `${integerPart}.${decimalPart}`.replace(/\.?0+$/, "")
