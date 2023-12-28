bde1dbfff193f98573f2373ecaec80f34b2a8c3c
bluecco <bluecco@gmail.com>
2023-12-28 15:33:27 +0100

    chore: add exports for notification components

--- packages/ui/src/main.ts
+++ packages/ui/src/main.ts
@@ -3,5 +3,15 @@ import "./styles.css"
 import { useAccount } from "./hooks/useAccount"
 import { ConnectButtonProvider } from "./components/WalletContext"
 import { StarknetkitButton } from "./components/Connect/StarknetkitButton"
+import { NotificationButton } from "./components/Notifications/NotificationButton"
+import { NotificationMenu } from "./components/Notifications/NotificationMenu"
+import { NotificationItem } from "./components/Notifications/NotificationItem"
 
-export { useAccount, ConnectButtonProvider, StarknetkitButton }
+export {
+  useAccount,
+  ConnectButtonProvider,
+  StarknetkitButton,
+  NotificationButton,
+  NotificationMenu,
+  NotificationItem,
+}
