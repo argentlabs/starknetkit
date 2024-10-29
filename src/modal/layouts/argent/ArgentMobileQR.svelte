7097abccb3c2ae8d8121bd5dfa6f23b58b1a4e37
Aleksandar Cakalic <aleksandar@argent.xyz>
2024-10-29 12:53:13 +0100

    chore: replace QR with loader, handle click

--- src/modal/layouts/argent/ArgentMobileQR.svelte
+++ src/modal/layouts/argent/ArgentMobileQR.svelte
@@ -1,17 +1,62 @@
-<script>
+<script lang="ts">
   import InstallWallet from "../../components/InstallWallet.svelte";
-  import QRCode from "./QRCode.svelte";
+
+  export let handleClick: () => void = () => {}
 </script>
+
+<style>
+  @keyframes dash {
+    0% {
+      stroke-dashoffset: 187;
+    }
+    50% {
+      stroke-dashoffset: 46.75;
+      transform: rotate(135deg);
+    }
+    100% {
+      stroke-dashoffset: 187;
+      transform: rotate(450deg);
+    }
+  }
+
+  @keyframes rotator {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(270deg);
+    }
+  }
+
+  .spinner {
+    color: #ff875b;
+    animation: rotator 1.4s linear infinite;
+  }
+
+  .spinner circle {
+    stroke: currentColor;
+    stroke-dasharray: 187;
+    stroke-dashoffset: 0;
+    transform-origin: center;
+    animation: dash 1.4s ease-in-out infinite;
+  }
+</style>
 
 <section class="flex flex-col gap-4">
   <div>
     <h3 class="mb-4 text-h5 text-primary font-semibold">Connect Argent mobile by<br />scanning QR code:</h3>
 
-    <div class="w-full flex justify-center">
-      <!-- TODO replace QR, slot or prop -->
-      <QRCode />
+    <div class="w-full flex justify-center items-center qr-code-slot h-[245px]">
+      <!--
+        @dev Spinner is replaced after loading by QR code;
+        Spinner and styles copied to be exactly the same as in argent-mobile-login,
+        since that one will appear once QR code is ready for it's loading state
+      -->
+      <svg class="spinner" viewBox="0 0 66 66" width="100px" height="100px" xmlns="http://www.w3.org/2000/svg">
+        <circle fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30" />
+      </svg>
     </div>
   </div>
 
-  <InstallWallet walletName="Argent" walletLink="#TODO" />
+  <InstallWallet walletName="Argent" handleClick={handleClick} />
 </section>
