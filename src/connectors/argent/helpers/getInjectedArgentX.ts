de4cae8e52144300e30d20c34b0ffb36daba1ddf
Aleksandar Cakalic <aleksandar@argent.xyz>
2024-11-01 10:11:29 +0100

    fix: case when `window` is `undefined` in `getInjectedArgentX`

--- src/connectors/argent/helpers/getInjectedArgentX.ts
+++ src/connectors/argent/helpers/getInjectedArgentX.ts
@@ -1,6 +1,9 @@
 import type { StarknetWindowObject } from "@starknet-io/types-js"
 
 export function getInjectedArgentX() {
+  if (typeof window === "undefined") {
+    return undefined
+  }
   return window?.starknet_argentX as
     | (StarknetWindowObject & {
         isInAppBrowser: boolean
