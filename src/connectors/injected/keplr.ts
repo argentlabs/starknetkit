d30b5eac687aa29ce5a148d95931323d7c064ad8
Aleksandar Cakalic <aleksandar@argent.xyz>
2025-02-20 12:08:43 +0100

    chore: add missing static methods to `metamask` and `keplr` connectors

--- src/connectors/injected/keplr.ts
+++ src/connectors/injected/keplr.ts
@@ -1,7 +1,17 @@
 import { InjectedConnector, type InjectedConnectorOptions } from "./index"
 
+const id = "keplr"
+
 export class Keplr extends InjectedConnector {
   constructor(options?: Omit<InjectedConnectorOptions, "id">) {
-    super({ options: { id: "keplr", ...options } })
+    super({ options: { id, ...options } })
+  }
+
+  static getInjectedWallet() {
+    return super.getInjectedWallet(id)
+  }
+
+  static isWalletInjected() {
+    return super.isWalletInjected(id)
   }
 }
